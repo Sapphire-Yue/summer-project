d10a409f2d49a1f59b5c6cd560eb92137d4b6db1
Penley-Wang <s11127103@cycu.edu.tw>
2024-10-01 21:13:44 +0800

    128->64

--- label_define.py
+++ label_define.py
@@ -33,7 +33,7 @@ datagen = ImageDataGenerator(
 # 載入訓練數據
 train_generator = datagen.flow_from_directory(
     'only_dataset/',  # 圖片資料夾
-    target_size=(128, 128),  # 圖片縮放至 64x64
+    target_size=(64, 64),  # 圖片縮放至 64x64
     batch_size=32,  # 批量大小
     class_mode='categorical',  # 多類別分類
     subset='training'  # 使用訓練集
@@ -42,7 +42,7 @@ train_generator = datagen.flow_from_directory(
 # 載入驗證數據
 validation_generator = datagen.flow_from_directory(
     'only_dataset/',  # 圖片資料夾
-    target_size=(128, 128),  # 圖片縮放至 64x64
+    target_size=(64, 64),  # 圖片縮放至 64x64
     batch_size=32,  # 批量大小
     class_mode='categorical',  # 多類別分類
     subset='validation'  # 使用驗證集
